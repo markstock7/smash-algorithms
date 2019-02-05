d241bb2b032c4e8dfbaf25dfe39de3583a0b8811
markstock <markstock7@hotmail.com>
2019-02-05 22:51:35 +0800

    optimize UnqiuePaths

--- src/main/scala/algorithms/dp/UniquePaths.scala
+++ src/main/scala/algorithms/dp/UniquePaths.scala
@@ -4,18 +4,16 @@ object UniquePaths {
   def solution(m: Int, n: Int): Int = {
     if (m < 1 || n < 1) return 0
     val d: Array[Array[Int]] = Array.fill(m ,n)(1)
-    for (i <- 1 until m)
-      for (j <- 1 until n)
-        d(i)(j) = d(i - 1)(j) + d(i)(j - 1)
+    for (i <- 1 until m; j <- 1 until n)
+      d(i)(j) = d(i - 1)(j) + d(i)(j - 1)
     d(m - 1)(n - 1)
   }
   
   def solutionII(m: Int, n: Int): Int = {
     if (m < 1 || n < 1) return 0
     val d: Array[Int] = Array.fill(n)(1)
-    for (i <- 1 until m)
-      for (j <- 1 until n)
-        d(j) = d(j) + d(j - 1)
+    for (i <- 1 until m; j <- 1 until n)
+      d(j) = d(j) + d(j - 1)
     d(n - 1)
   }
   
